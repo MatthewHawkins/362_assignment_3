b0dbba4fdcfec49806da30a9c14fe09c8772a890
Matthew Hawkins <matthew.hawkins1115@gmail.com>
2021-05-22 15:54:14 -0700

    Fixed error

--- test_leap_year.py
+++ test_leap_year.py
@@ -4,22 +4,22 @@ import unittest
 
 
 class TestCase(unittest.TestCase):
-    def test_leap(self):
+    def test_leap1(self):
         self.assertEqual(leapYr.leap_year(3), False)
     
-    def test_leap(self):
+    def test_leap2(self):
         self.assertEqual(leapYr.leap_year(2004), True)
     
-    def test_leap(self):
+    def test_leap3(self):
         self.assertEqual(leapYr.leap_year(400), True)
     
-    def test_leap(self):
+    def test_leap4(self):
         self.assertEqual(leapYr.leap_year(2021), False)
 
-    def test_leap(self):
+    def test_leap5(self):
         self.assertRaises(TypeError, leapYr.leap_year("hello"))
     
-    def test_leap(self):
+    def test_leap6(self):
         self.assertEqual(leapYr.leap_year(400), False) #Should fail!
 
 
