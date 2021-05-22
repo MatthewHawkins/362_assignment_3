bb3454549cb88736cb352ee58df7ad3e8a9bdd2a
Matthew Hawkins <matthew.hawkins1115@gmail.com>
2021-05-22 15:50:14 -0700

    tests coded and run

--- test_leap_year.py
+++ test_leap_year.py
@@ -1,10 +1,30 @@
+from typing import Type
 import leapYr
 import unittest
 
 
 class TestCase(unittest.TestCase):
     def test_leap(self):
-        self.assertEqual(leapYr.leap_year(2000), True)
+        self.assertEqual(leapYr.leap_year(3), False)
+    
+    def test_leap(self):
+        self.assertEqual(leapYr.leap_year(2004), True)
+    
+    def test_leap(self):
+        self.assertEqual(leapYr.leap_year(400), True)
+    
+    def test_leap(self):
+        self.assertEqual(leapYr.leap_year(2021), False)
+
+    def test_leap(self):
+        self.assertRaises(TypeError, leapYr.leap_year("hello"))
+    
+    def test_leap(self):
+        self.assertEqual(leapYr.leap_year(400), False) #Should fail!
+
+
+
+
 
 
 if __name__ == "__main__":
